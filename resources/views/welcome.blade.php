d9561d05dba2e391b176765595bd7db06fa9fe54
Marista Adelia Putri <maristaadeliap@gmail.com>
2024-05-05 22:36:29 +0700

    finalisasi halaman welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar Controller & View pada Laravel</title>
+    <title>MAHASISWA C, BAGIAN KEDUA</title>
     @vite('resources/sass/app.scss')
 </head>
 <body>
     <div class="container text-center my-5">
         <h1 class="mb-4">Belajar Controller & View pada Laravel</h1>
-        {{-- Ini kode buka gambar --}}
+
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/Sky.jpeg') }}" alt="image">
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
